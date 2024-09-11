65ece1edd0b0c20d2065c29fb801b7d2ade892a0
yyxff <salmontwosday@gmail.com>
2024-09-10 21:28:13 -0400

    use CSA_32b now, build from RCA_4b

---
+++ projects/proj1/CSA_8b.v
@@ -0,0 +1,27 @@
+module CSA_8b(a, b, cin, cout, s);
+	//port
+	input [7:0] a, b;
+	input cin;
+	output [7:0] s;
+	output cout;
+	
+	//wires
+	wire [3:0] s_c0, s_c1;
+	wire cout_c0, cout_c1, cout_low;
+	
+	
+	//high part when cin=0
+	RCA_4b add_rca(.a(a[7:4]), .b(b[7:4]), .cin(1'b0), .cout(cout_c0), .s(s_c0));
+	
+	//high part when cin=1
+	RCA_4b sub_rca(.a(a[7:4]), .b(b[7:4]), .cin(1'b1), .cout(cout_c1), .s(s_c1));
+	
+	//low part
+	RCA_4b low_rca(.a(a[3:0]), .b(b[3:0]), .cin(cin), .cout(cout_low), .s(s[3:0]));
+	
+	//mux
+	assign s[7:4] = (cout_low == 1'b0)? s_c0: s_c1;
+	assign cout = (cout_low == 1'b0)? cout_c0: cout_c1;
+
+	
+endmodule
--- projects/proj1/alu.v
+++ projects/proj1/alu.v
@@ -9,50 +9,39 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
    // YOUR CODE HERE //
 	
 	
-	// use same RCA
-	
-	//carry in and out
-	wire wo0, wo1, wo2, wo3, wo4,wo5, wo6, wo7;
-	// result
-	wire [31:0] add_result;
+	// wires
+	wire [31:0] b, result;
+	// cin cout wire
+	wire cin_now, cout_now;
+	// wire to calculate overflow
+	wire of_add0, of_add1, of_sub0, of_sub1;
 	// overfloe sign
 	wire add_overflow, sub_overflow;
-	// operandB into RCA
-	wire [31:0] operandB;
-	// temp wire to calculate overflow
-	wire of_add0, of_add1, of_sub0, of_sub1;
 	
 	
-	// input depends on opcode
-	assign operandB = (ctrl_ALUopcode == 2'b00000)? data_operandB: ~data_operandB;
-	assign cin_now = (ctrl_ALUopcode == 2'b00000)? 1'b0: 1'b1;
+	// input b if add, input notb if sub
+	assign b = (ctrl_ALUopcode == 2'b00000)? data_operandB: ~data_operandB;
+	// input cin = 0 if add, input cin = 1 if sub
+	assign cin_now = (ctrl_ALUopcode == 2'b00000)? 1'b0:1'b1;
+	
+	// CSA_32b
+	CSA_32b csa(.a(data_operandA), .b(b), .cin(cin_now), .cout(cout_now), .s(result));
 	
 	
-	// RCA
-	RCA_4b R0_a(.a(data_operandA[3:0]), .b(operandB[3:0]), .cin(cin_now), .cout(wo0), .s(add_result[3:0]));
-	RCA_4b R1_a(.a(data_operandA[7:4]), .b(operandB[7:4]), .cin(wo0), .cout(wo1), .s(add_result[7:4]));
-	RCA_4b R2_a(.a(data_operandA[11:8]), .b(operandB[11:8]), .cin(wo1), .cout(wo2), .s(add_result[11:8]));
-	RCA_4b R3_a(.a(data_operandA[15:12]), .b(operandB[15:12]), .cin(wo2), .cout(wo3), .s(add_result[15:12]));
-	RCA_4b R4_a(.a(data_operandA[19:16]), .b(operandB[19:16]), .cin(wo3), .cout(wo4), .s(add_result[19:16]));
-	RCA_4b R5_a(.a(data_operandA[23:20]), .b(operandB[23:20]), .cin(wo4), .cout(wo5), .s(add_result[23:20]));
-	RCA_4b R6_a(.a(data_operandA[27:24]), .b(operandB[27:24]), .cin(wo5), .cout(wo6), .s(add_result[27:24]));
-	RCA_4b R7_a(.a(data_operandA[31:28]), .b(operandB[31:28]), .cin(wo6), .cout(wo7), .s(add_result[31:28]));
-	
-	
+	// check overflow 
 	// add overflow
-	and(of_add0, ~data_operandA[31], ~data_operandB[31], add_result[31]);
-	and(of_add1, data_operandA[31], data_operandB[31], ~add_result[31]);
+	and(of_add0, ~data_operandA[31], ~data_operandB[31], result[31]);
+	and(of_add1, data_operandA[31], data_operandB[31], ~result[31]);
 	or(add_overflow, of_add0, of_add1);
 	
-	
 	// sub overflow
-	and(of_sub0, data_operandA[31], ~data_operandB[31], ~add_result[31]);
-	and(of_sub1, ~data_operandA[31], data_operandB[31], add_result[31]);
+	and(of_sub0, data_operandA[31], ~data_operandB[31], ~result[31]);
+	and(of_sub1, ~data_operandA[31], data_operandB[31], result[31]);
 	or(sub_overflow, of_sub0, of_sub1);
 	
 	
 	// result
-	assign data_result = add_result;
+	assign data_result = result;
 	assign overflow = (ctrl_ALUopcode == 2'b00000)? add_overflow: sub_overflow; 
 	
 endmodule
