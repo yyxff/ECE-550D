cbf14c9d78defec7c53cf6b18bcc417b47438462
yyxff <salmontwosday@gmail.com>
2024-09-15 21:06:18 -0400

    added generate block

--- projects/proj1/alu.v
+++ projects/proj1/alu.v
@@ -10,7 +10,7 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	
 	
 	// wires
-	wire [31:0] b;
+	wire [31:0] b, notb;
 	// cin cout wire
 	wire cin_now, cout_now;
 	// wire to calculate overflow
@@ -19,28 +19,43 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	wire add_overflow, sub_overflow;
 	
 	
+	//used to calculate overflow
+	wire nota31, notres31;
+	not(nota31, data_operandA[31]);
+	not(notres31, data_result[31]);
+	
 	// input b if add, input notb if sub
-	assign b = (ctrl_ALUopcode == 2'b00000)? data_operandB: ~data_operandB;
+	// generate notb instead of many not gate statements
+	genvar i;
+	generate
+		for (i = 0; i<32;i=i+1) begin: get_notb
+			not(notb[i], data_operandB[i]);
+		end
+	endgenerate
+
+	assign b = (ctrl_ALUopcode == 2'b00000)? data_operandB:notb;
+	
+	
 	// input cin = 0 if add, input cin = 1 if sub
 	assign cin_now = (ctrl_ALUopcode == 2'b00000)? 1'b0:1'b1;
 	
-	// CSA_32b
+	// do add or sub by CSA_32b
 	CSA_32b csa(.a(data_operandA), .b(b), .cin(cin_now), .cout(cout_now), .s(data_result));
 	
 	
 	// check overflow 
 	// add overflow
-	and(of_add0, ~data_operandA[31], ~data_operandB[31], data_result[31]);
-	and(of_add1, data_operandA[31], data_operandB[31], ~data_result[31]);
+	and(of_add0, nota31, notb[31], data_result[31]);
+	and(of_add1, data_operandA[31], data_operandB[31], notres31);
 	or(add_overflow, of_add0, of_add1);
 	
 	// sub overflow
-	and(of_sub0, data_operandA[31], ~data_operandB[31], ~data_result[31]);
-	and(of_sub1, ~data_operandA[31], data_operandB[31], data_result[31]);
+	and(of_sub0, data_operandA[31], notb[31], notres31);
+	and(of_sub1, nota31, data_operandB[31], data_result[31]);
 	or(sub_overflow, of_sub0, of_sub1);
 	
 	
-	// result
+	// overflow result
 	assign overflow = (ctrl_ALUopcode == 2'b00000)? add_overflow: sub_overflow; 
 	
 endmodule
