6dec37e613f961136d5d3c15a22b010a2783aa34
yyxff <salmontwosday@gmail.com>
2024-09-15 20:22:10 -0400

    remove the assign statements

--- projects/proj1/alu.v
+++ projects/proj1/alu.v
@@ -10,7 +10,7 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	
 	
 	// wires
-	wire [31:0] b, result;
+	wire [31:0] b;
 	// cin cout wire
 	wire cin_now, cout_now;
 	// wire to calculate overflow
@@ -25,23 +25,22 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	assign cin_now = (ctrl_ALUopcode == 2'b00000)? 1'b0:1'b1;
 	
 	// CSA_32b
-	CSA_32b csa(.a(data_operandA), .b(b), .cin(cin_now), .cout(cout_now), .s(result));
+	CSA_32b csa(.a(data_operandA), .b(b), .cin(cin_now), .cout(cout_now), .s(data_result));
 	
 	
 	// check overflow 
 	// add overflow
-	and(of_add0, ~data_operandA[31], ~data_operandB[31], result[31]);
-	and(of_add1, data_operandA[31], data_operandB[31], ~result[31]);
+	and(of_add0, ~data_operandA[31], ~data_operandB[31], data_result[31]);
+	and(of_add1, data_operandA[31], data_operandB[31], ~data_result[31]);
 	or(add_overflow, of_add0, of_add1);
 	
 	// sub overflow
-	and(of_sub0, data_operandA[31], ~data_operandB[31], ~result[31]);
-	and(of_sub1, ~data_operandA[31], data_operandB[31], result[31]);
+	and(of_sub0, data_operandA[31], ~data_operandB[31], ~data_result[31]);
+	and(of_sub1, ~data_operandA[31], data_operandB[31], data_result[31]);
 	or(sub_overflow, of_sub0, of_sub1);
 	
 	
 	// result
-	assign data_result = result;
 	assign overflow = (ctrl_ALUopcode == 2'b00000)? add_overflow: sub_overflow; 
 	
 endmodule
