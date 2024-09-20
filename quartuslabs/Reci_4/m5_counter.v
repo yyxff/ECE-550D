e431d8aa818ee2c7115c1cccaa5122c0e16a9c69
yyxff <salmontwosday@gmail.com>
2024-09-19 23:30:58 -0400

    Done moore and output

--- quartuslabs/Reci_4/m5_counter.v
+++ quartuslabs/Reci_4/m5_counter.v
@@ -1,13 +1,21 @@
 module m5_counter(
 	clk,
 	reset,
-	level,
+	w,
 	now_me,
 	next_me,
-	count_me);
+	count_me,
+	out_me,
+	now_mo,
+	next_mo,
+	count_mo,
+	out_mo);
 
-	input clk,reset,level;
+	input clk,reset,w;
 	output [2:0] now_me, next_me, count_me;
+	output out_me;
+	output [2:0] now_mo, next_mo, count_mo;
+	output out_mo;
 	
 	localparam [2:0]
 		me_0 = 3'b000,
@@ -20,6 +28,7 @@ module m5_counter(
 		me_7 = 3'b111;
 
 	reg [2:0] now_me, next_me, count_me;
+	reg out_me;
 	
 	localparam [2:0]
 		mo_0 = 3'b000,
@@ -31,7 +40,8 @@ module m5_counter(
 		mo_6 = 3'b110,
 		mo_7 = 3'b111;
 
-	reg [2:0] now_mo, next_mo;
+	reg [2:0] now_mo, next_mo, count_mo;
+	reg out_mo;
 	
 	always @(negedge clk, posedge reset)begin
 		if(reset)begin
@@ -46,60 +56,129 @@ module m5_counter(
 	
 	
 	//Mealy
-	always @(now_me, level)begin
+	always @(now_me, w)begin
 	
-		// if level is 0, keep current state
+		// if w is 0, keep current state
 		next_me <= now_me;
 		count_me <= now_me;
+		out_me <= 1'b0;
 		
-		// if level is 1, transit the state by case
+		// if w is 1, transit the state by case
 		case(now_me)
 			me_0:
-			if(level)begin
+			if(w)begin
 				next_me <= me_1;
 				count_me <= 3'b001;
 			end
 			
 			me_1:
-			if(level)begin
+			if(w)begin
 				next_me <= me_2;
 				count_me <= 3'b010;
 			end
 			
 			me_2:
-			if(level)begin
+			if(w)begin
 				next_me <= me_3;
 				count_me <= 3'b011;
 			end
 			
 			me_3:
-			if(level)begin
+			if(w)begin
 				next_me <= me_4;
 				count_me <= 3'b100;
 			end
 			
-			me_4:
-			if(level)begin
+			me_4:begin
+			out_me <= 1'b1;
+			if(w)begin
 				next_me <= me_0;
 				count_me <= 3'b000;
 			end
+			end
 			
 			me_5:
-			if(level)begin
+			if(w)begin
 				next_me <= me_0;
 				count_me <= 3'b000;
 			end
 			
 			me_6:
-			if(level)begin
+			if(w)begin
 				next_me <= me_0;
 				count_me <= 3'b000;
 			end
 			
 			me_7:
-			if(level)begin
+			if(w)begin
 				next_me <= me_0;
 				count_me <= 3'b000;
+			end
+		endcase
+	end
+	
+	
+	//Moore 
+	always @(now_mo, w)begin
+		next_mo <= now_mo;
+		out_mo <= 1'b0;
+		
+		case(now_mo)
+			mo_0:begin
+			count_mo <= mo_0;
+			if(w)begin
+				next_mo <= mo_1;
+			end
+			end
+			
+			mo_1:begin
+			count_mo <= mo_1;
+			if(w)begin
+				next_mo <= mo_2;
+			end
+			end
+			
+			mo_2:begin
+			count_mo <= mo_2;
+			if(w)begin
+				next_mo <= mo_3;
+			end
+			end
+			
+			mo_3:begin
+			count_mo <= mo_3;
+			if(w)begin
+				next_mo <= mo_4;
+			end
+			end
+			
+			mo_4:begin
+			out_mo <= 1'b1;
+			count_mo <= mo_4;
+			if(w)begin
+				next_mo <= mo_0;
+			end
+			end
+			
+			mo_5:begin
+			count_mo <= mo_5;
+			if(w)begin
+				next_mo <= mo_0;
+			end
+			end
+			
+			mo_6:begin
+			count_mo <= mo_6;
+			if(w)begin
+				next_mo <= mo_0;
+			end
+			end
+			
+			mo_7:begin
+			count_mo <= mo_7;
+			if(w)begin
+				next_mo <= mo_0;
+			end
 			end
 		endcase
 	end
--- quartuslabs/Reci_4/m5_counter_tb.v
+++ quartuslabs/Reci_4/m5_counter_tb.v
@@ -4,10 +4,11 @@ module m5_counter_tb();
 	reg clk;
 	reg reset;
 	
-	wire [2:0] count_me;
-	wire [2:0] now_me,next_me;
+	wire [2:0] count_me, count_mo;
+	wire [2:0] now_me,next_me, now_mo, next_mo;
+	wire out_me, out_mo;
 	
-	m5_counter test_m5_counter(.clk(clk), .reset(reset),.level(w),.now_me(now_me), .next_me(next_me), .count_me(count_me));
+	m5_counter test_m5_counter(.clk(clk), .reset(reset),.w(w),.now_me(now_me), .next_me(next_me), .count_me(count_me), .out_me(out_me), .now_mo(now_mo), .next_mo(next_mo), .count_mo(count_mo), .out_mo(out_mo));
 	
 	initial begin
 		reset = 1'b1;
