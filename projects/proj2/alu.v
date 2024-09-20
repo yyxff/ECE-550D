f358708abeae88139e33d752f6a4a449fae4da0e
yyxff <salmontwosday@gmail.com>
2024-09-20 09:19:02 -0400

    selcet result by opcode

--- projects/proj2/alu.v
+++ projects/proj2/alu.v
@@ -10,7 +10,7 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	
 	
 	// wires
-	wire [31:0] b, notb;
+	wire [31:0] b, notb, cal_result;
 	// cin cout wire
 	wire cin_now, cout_now;
 	// wire to calculate overflow
@@ -24,6 +24,23 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	not(nota31, data_operandA[31]);
 	not(notres31, data_result[31]);
 	
+	
+	/*
+	opcode
+	*/
+	wire op_add, op_sub, op_and, op_or, op_sll, op_sra;
+	and(op_add, ~ctrl_ALUopcode[2], ~ctrl_ALUopcode[1], ~ctrl_ALUopcode[0]);
+	and(op_sub, ~ctrl_ALUopcode[2], ~ctrl_ALUopcode[1], ctrl_ALUopcode[0]);
+	and(op_and, ~ctrl_ALUopcode[2], ctrl_ALUopcode[1], ~ctrl_ALUopcode[0]);
+	and(op_or, ~ctrl_ALUopcode[2], ctrl_ALUopcode[1], ctrl_ALUopcode[0]);
+	and(op_sll, ctrl_ALUopcode[2], ~ctrl_ALUopcode[1], ~ctrl_ALUopcode[0]);
+	and(op_sra, ctrl_ALUopcode[2], ~ctrl_ALUopcode[1], ctrl_ALUopcode[0]);
+	
+	
+	/*
+	add/sub
+	*/
+	
 	// input b if add, input notb if sub
 	// generate notb instead of many not gate statements
 	genvar i;
@@ -33,15 +50,20 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 		end
 	endgenerate
 
-	assign b = (ctrl_ALUopcode[0])? notb: data_operandB;
+	assign b = (op_sub)? notb: data_operandB;
 	
 	
 	// input cin = 0 if add, input cin = 1 if sub
-	assign cin_now = (ctrl_ALUopcode[0])? 1'b1:1'b0;
+	assign cin_now = (op_sub)? 1'b1:1'b0;
 	
 	// do add or sub by CSA_32b
-	CSA_32b csa(.a(data_operandA), .b(b), .cin(cin_now), .cout(cout_now), .s(data_result));
+	CSA_32b csa(.a(data_operandA), .b(b), .cin(cin_now), .cout(cout_now), .s(cal_result));
+	//assign data_result = (op_add)? cal_result: dataresult;
+	//assign data_result = (op_sub)? cal_result: dataresult;
 	
+	/*
+	overflow
+	*/
 	
 	// check overflow 
 	// add overflow
@@ -56,6 +78,45 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	
 	
 	// overflow result
-	assign overflow = (ctrl_ALUopcode[0])? sub_overflow: add_overflow; 
+	assign overflow = (op_sub)? sub_overflow: add_overflow; 
+	
+	
+	/*
+	AND
+	*/
+	
+	// wires
+	wire [31:0] andAB;
+	
+	// and gates
+	generate
+		for (i = 0; i<32;i=i+1) begin: get_AND
+			and(andAB[i], data_operandA[i], data_operandB[i]);
+		end
+	endgenerate
+	
+	//assign data_result = (op_and)? andAB: data_result;
+	
+	/*
+	AND
+	*/
+	
+	// wires
+	wire [31:0] orAB;
+	
+	// and gates
+	generate
+		for (i = 0; i<32;i=i+1) begin: get_OR
+			or(orAB[i], data_operandA[i], data_operandB[i]);
+		end
+	endgenerate
+	
+	/*
+	data_result
+	*/
+	assign data_result = (op_add)? cal_result:
+								(op_sub)? cal_result:
+								(op_and)? andAB:
+								(op_or)?  orAB:data_result;
 	
 endmodule
