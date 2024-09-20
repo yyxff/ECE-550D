a5efa105a0518a3491392d96fafbd77f65da04b6
yyxff <salmontwosday@gmail.com>
2024-09-19 22:25:40 -0400

    Done mealy one!

--- quartuslabs/Reci_4/m5_counter.v
+++ quartuslabs/Reci_4/m5_counter.v
@@ -2,10 +2,12 @@ module m5_counter(
 	clk,
 	reset,
 	level,
-	now_me);
+	now_me,
+	next_me,
+	count_me);
 
 	input clk,reset,level;
-	output [2:0] now_me;
+	output [2:0] now_me, next_me, count_me;
 	
 	localparam [2:0]
 		me_0 = 3'b000,
@@ -17,62 +19,87 @@ module m5_counter(
 		me_6 = 3'b110,
 		me_7 = 3'b111;
 
-	reg [2:0] now_me, next_me;
+	reg [2:0] now_me, next_me, count_me;
 	
-	always @(posedge clk, posedge reset)begin
+	localparam [2:0]
+		mo_0 = 3'b000,
+		mo_1 = 3'b001,
+		mo_2 = 3'b010,
+		mo_3 = 3'b011,
+		mo_4 = 3'b100,
+		mo_5 = 3'b101,
+		mo_6 = 3'b110,
+		mo_7 = 3'b111;
+
+	reg [2:0] now_mo, next_mo;
+	
+	always @(negedge clk, posedge reset)begin
 		if(reset)begin
 			now_me <= me_0;
+			now_mo <= mo_0;
 		end
 		else begin
 			now_me <= next_me;
+			now_mo <= next_mo;
 		end
 	end
 	
+	
+	//Mealy
 	always @(now_me, level)begin
 	
 		// if level is 0, keep current state
 		next_me <= now_me;
+		count_me <= now_me;
 		
 		// if level is 1, transit the state by case
 		case(now_me)
 			me_0:
 			if(level)begin
 				next_me <= me_1;
+				count_me <= 3'b001;
 			end
 			
 			me_1:
 			if(level)begin
 				next_me <= me_2;
+				count_me <= 3'b010;
 			end
 			
 			me_2:
 			if(level)begin
 				next_me <= me_3;
+				count_me <= 3'b011;
 			end
 			
 			me_3:
 			if(level)begin
 				next_me <= me_4;
+				count_me <= 3'b100;
 			end
 			
 			me_4:
 			if(level)begin
 				next_me <= me_0;
+				count_me <= 3'b000;
 			end
 			
 			me_5:
 			if(level)begin
 				next_me <= me_0;
+				count_me <= 3'b000;
 			end
 			
 			me_6:
 			if(level)begin
 				next_me <= me_0;
+				count_me <= 3'b000;
 			end
 			
 			me_7:
 			if(level)begin
 				next_me <= me_0;
+				count_me <= 3'b000;
 			end
 		endcase
 	end
--- quartuslabs/Reci_4/m5_counter_tb.v
+++ quartuslabs/Reci_4/m5_counter_tb.v
@@ -4,9 +4,10 @@ module m5_counter_tb();
 	reg clk;
 	reg reset;
 	
-	wire [2:0] count;
+	wire [2:0] count_me;
+	wire [2:0] now_me,next_me;
 	
-	m5_counter test_m5_counter(.clk(clk), .reset(reset),.level(w),.now_me(count));
+	m5_counter test_m5_counter(.clk(clk), .reset(reset),.level(w),.now_me(now_me), .next_me(next_me), .count_me(count_me));
 	
 	initial begin
 		reset = 1'b1;
