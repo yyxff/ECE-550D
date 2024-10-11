adbe33c559bd878fd1800eb6bb91d0814db796e9
yyxff <salmontwosday@gmail.com>
2024-10-11 10:10:00 -0400

    reci_7 key to lcd

---
+++ quartuslabs/Reci_7/PS2_Interface.v
@@ -0,0 +1,41 @@
+module PS2_Interface(inclock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, last_data_received);
+
+	input 			inclock, resetn;
+	inout 			ps2_clock, ps2_data;
+	output 			ps2_key_pressed;
+	output 	[7:0] 	ps2_key_data;
+	output 	[7:0] 	last_data_received;
+
+	// Internal Registers
+	reg			[7:0]	last_data_received;	
+	
+	always @(posedge inclock)
+	begin
+		if (resetn == 1'b0)
+			last_data_received <= 8'h00;
+		else if (ps2_key_pressed == 1'b1)
+			last_data_received <= ps2_key_data;
+		
+		case(last_data_received)
+			8'h23:last_data_received <= 8'h44;
+			
+			8'h2B:last_data_received <= 8'h46;
+			
+			8'h42:last_data_received <= 8'h4B;
+			
+			8'h22:last_data_received <= 8'h58;
+			
+			8'h35:last_data_received <= 8'h59;
+		
+		endcase
+	end
+	
+	PS2_Controller PS2 (.CLOCK_50 			(inclock),
+						.reset 				(~resetn),
+						.PS2_CLK			(ps2_clock),
+						.PS2_DAT			(ps2_data),		
+						.received_data		(ps2_key_data),
+						.received_data_en	(ps2_key_pressed)
+						);
+
+endmodule
